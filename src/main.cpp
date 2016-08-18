7242c354e7417e278ed6e0baca3155b15a0e5f79
dkurtaev <dmitry.kurtaev@gmail.com>
2016-08-18 23:44:36 +0300

    Two windows using separate ids

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,64 @@
 #include <GL/freeglut.h>
 
-void init();
-void display();
+void display_first();
+void display_second();
 void reshape(int width, int height);
+
+int first_window_id;
+int second_window_id;
 
 int main(int argc, char** argv) {
   // GLUT initialization.
   glutInit(&argc, argv);
 
-  // Window initialization.
+  // Windows initialization.
   glutInitWindowSize(324, 324);  // Width, height of window.
   glutInitWindowPosition(0, 0);  // (x, y) position of window from top-left.
-  int window_id = glutCreateWindow("Single view");
+  first_window_id = glutCreateWindow("First view");
+  glutInitWindowSize(324, 324);
+  glutInitWindowPosition(500, 0);
+  second_window_id = glutCreateWindow("Second view");
 
-  // Set callbacks.
-  glutDisplayFunc(display);
-  glutIdleFunc(display);
+  // Set callbacks for first window.
+  glutSetWindow(first_window_id);
+  glutDisplayFunc(display_first);
+  glutIdleFunc(display_first);
   glutReshapeFunc(reshape);
 
-  // Start GLUT's internal loop. Exit by window closing.
+  // Set callbacks for second window.
+  glutSetWindow(second_window_id);
+  glutDisplayFunc(display_second);
+  glutIdleFunc(display_second);
+  glutReshapeFunc(reshape);
+
+  // Start GLUT's internal loop. Exit by one of windows is closing.
   glutMainLoop();
   return 0;
 }
 
-void init() {
-  glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-}
-
-void display() {
+void display_first() {
+  glutSetWindow(first_window_id);
   glClear(GL_COLOR_BUFFER_BIT);
 
-  glColor3ub(0xCA, 0xFF, 0xEE);  // Hex just for fun. Color is (202, 255, 238).
+  glColor3ub(0xCA, 0xFF, 0xEE);
   glBegin(GL_TRIANGLES);
     glVertex2f(0.0f, 0.5f);
     glVertex2f(-0.5f, -0.5f);
     glVertex2f(0.5f, -0.5f);
+  glEnd();
+
+  glutSwapBuffers();
+}
+
+void display_second() {
+  glutSetWindow(second_window_id);
+  glClear(GL_COLOR_BUFFER_BIT);
+
+  glColor3ub(0xCA, 0xFF, 0xEE);
+  glBegin(GL_TRIANGLES);
+    glVertex2f(0.0f, -0.5f);
+    glVertex2f(0.5f, 0.5f);
+    glVertex2f(-0.5f, 0.5f);
   glEnd();
 
   glutSwapBuffers();
