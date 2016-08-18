3c908a1365d0400f6794ac097796fc8fd3bb333c
dkurtaev <dmitry.kurtaev@gmail.com>
2016-08-18 20:18:23 +0300

    Empty single window

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,45 @@
+#include <GL/freeglut.h>
+
+static const unsigned kInitViewWidth = 324;
+static const unsigned kInitViewHeight = 324;
+static const unsigned kInitViewPositionX = 0;
+static const unsigned kInitViewPositionY = 0;
+static const char* kViewTitle = "Single view";
+
+void init();
+
+void display();
+
+void reshape(int width, int height);
+
 int main(int argc, char** argv) {
+  // GLUT initialization.
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+
+  // Window initialization.
+  glutInitWindowSize(kInitViewWidth, kInitViewHeight);
+  glutInitWindowPosition(kInitViewPositionX, kInitViewPositionY);
+  int window_id = glutCreateWindow(kViewTitle);
+
+  // Set callbacks.
+  glutDisplayFunc(display);
+  glutIdleFunc(display);
+  glutReshapeFunc(reshape);
+
+  glutMainLoop();
   return 0;
+}
+
+void init() {
+  glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+}
+
+void display() {
+  glClear(GL_COLOR_BUFFER_BIT);
+  glutSwapBuffers();
+}
+
+void reshape(int width, int height) {
+  glViewport(0, 0, width, height);
 }
