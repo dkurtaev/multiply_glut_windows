2b7c6a82e9e7ac20c2cca9dbd655ce856bb17709
dkurtaev <dmitry.kurtaev@gmail.com>
2016-08-18 22:32:14 +0300

    Added triangle drawing

--- src/main.cpp
+++ src/main.cpp
@@ -29,6 +29,14 @@ void init() {
 
 void display() {
   glClear(GL_COLOR_BUFFER_BIT);
+
+  glColor3ub(0xCA, 0xFF, 0xEE);  // Hex just for fun. Color is (202, 255, 238).
+  glBegin(GL_TRIANGLES);
+    glVertex2f(0.0f, 0.5f);
+    glVertex2f(-0.5f, -0.5f);
+    glVertex2f(0.5f, -0.5f);
+  glEnd();
+
   glutSwapBuffers();
 }
 
