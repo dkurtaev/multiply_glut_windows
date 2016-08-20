1b326f1bd9c681d58a3df8da7f48feac53fefc89
Dmitry Kurtaev <dmitry.kurtaev@gmail.com>
2016-08-20 16:01:20 +0300

    Windows initialization order changed

--- src/main.cpp
+++ src/main.cpp
@@ -8,28 +8,25 @@ int first_window_id;
 int second_window_id;
 
 int main(int argc, char** argv) {
-  // GLUT initialization.
-  glutInit(&argc, argv);
+  glutInit(&argc, argv);  // GLUT initialization.
 
-  // Windows initialization.
+  // First window initialization.
   glutInitWindowSize(324, 324);  // Width, height of window.
   glutInitWindowPosition(0, 0);  // (x, y) position of window from top-left.
   first_window_id = glutCreateWindow("First view");
+
+  glutDisplayFunc(display_first);
+  glutReshapeFunc(reshape);
+  glutIdleFunc(display_first);
+
+  // Second window initialization.
   glutInitWindowSize(324, 324);
   glutInitWindowPosition(500, 0);
   second_window_id = glutCreateWindow("Second view");
 
-  // Set callbacks for first window.
-  glutSetWindow(first_window_id);
-  glutDisplayFunc(display_first);
-  glutIdleFunc(display_first);
-  glutReshapeFunc(reshape);
-
-  // Set callbacks for second window.
-  glutSetWindow(second_window_id);
   glutDisplayFunc(display_second);
-  glutIdleFunc(display_second);
   glutReshapeFunc(reshape);
+  glutIdleFunc(display_second);
 
   // Start GLUT's internal loop. Exit by one of windows is closing.
   glutMainLoop();
